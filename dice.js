5e5e27b54968aebc665013cbbb5437e1484019bb
ganeshgane05 <98518726+ganeshgane05@users.noreply.github.com>
2023-08-23 07:16:51 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,11 +1,11 @@
 var RandomNumber1 = Math.floor(Math.random() * 6) + 1;
 var RandomDice = "dice" + RandomNumber1 + ".png";
-var RandomImage = "/img/dice/" + RandomDice;
+var RandomImage = "dice/" + RandomDice;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", RandomImage);
 var image2 = document.querySelectorAll("img")[1];
 var RandomNumber2 = Math.floor(Math.random() * 6) + 1;
-var RandomImage1 = "/img/dice/dice" + RandomNumber2 + ".png";
+var RandomImage1 = "dice/dice" + RandomNumber2 + ".png";
 image2.setAttribute("src", RandomImage1);
 
 if (RandomNumber1 > RandomNumber2) {
